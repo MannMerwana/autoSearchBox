1d70b7b38e1b64b3564b168c2f6e6c20f94ada2f
Mann Merwana <mann.merwana27@gmail.com>
2025-04-17 17:10:05 +0530

    Optimized The Search Engine by Implementing Caching and Debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ function App() {
   const [searchInput,setSearchInput] = useState('');
   const [results,setResults] = useState([]);
   const [showResults,setShowResults] = useState(false);
+  const [cache,setCache] = useState({});//state to implement and handle cache to cache api calls
 
 
   const fetchData = async() => {
+    //if data is present in cache,setResults to the input
+    if(cache[searchInput]){
+      console.log(`CACHE RETURNED ${searchInput}`);
+      setResults(cache[searchInput])
+      return;
+    }
+    //if data is not present in the cache,then only make an api call
     const timeTakenForEachApiCallInMs = searchInput;
     console.log(`API Call ${timeTakenForEachApiCallInMs}`);
     const resData = await fetch(
@@ -18,6 +26,8 @@ function App() {
 
     const jsonData = await resData.json();
     setResults(jsonData?.recipes);
+    //                            here key is Mango as ex and jsondata is the result that we have got.
+    setCache((prev) => ({ ...prev, [searchInput]: jsonData?.recipes }));
   }
 //debouncing to reduce api calls.
   useEffect (()=>{
